e476010a6bd15a6ecf42c456345ac183b098892a
Amy Rafferty <amyfrances.r@gmail.com>
2019-01-15 14:30:17 -0700

    Updates

---
+++ precache-manifest.c88776ee2f06535602e45ce8692146a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea79587df1b89aef09e0",
+    "url": "/fcc-markdown-previewer/static/js/runtime~main.ea79587d.js"
+  },
+  {
+    "revision": "539cfd0a12c1a8f78247",
+    "url": "/fcc-markdown-previewer/static/js/main.539cfd0a.chunk.js"
+  },
+  {
+    "revision": "e61e7da9faf4142fe101",
+    "url": "/fcc-markdown-previewer/static/js/1.e61e7da9.chunk.js"
+  },
+  {
+    "revision": "539cfd0a12c1a8f78247",
+    "url": "/fcc-markdown-previewer/static/css/main.0fbaac98.chunk.css"
+  },
+  {
+    "revision": "3cc2b4b77d0ebb5c0e1da647c2fa8e6d",
+    "url": "/fcc-markdown-previewer/index.html"
+  }
+];
